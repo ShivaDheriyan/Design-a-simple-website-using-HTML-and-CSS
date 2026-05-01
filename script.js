e61ccdd6a528742566652bd10b33cb6d09977a0e
ShivaDheriyan <shivadheriyan@gmail.com>
2026-05-02 03:10:32 +0530

    New work in this Website

---
+++ script.js
@@ -0,0 +1,22 @@
+//Swiper js
+
+
+
+// Nav Open Close
+const body = document.querySelector('body'),
+      navMenu = body.querySelector('.menu-content'),
+      navOpenBtn = body.querySelector('.navOpen-btn'),
+      navCloseBtn = navMenu.querySelector('.navClose-btn');
+
+if(navMenu && navOpenBtn){
+    navOpenBtn.addEventListener("click", () =>{
+        navMenu.classList.add("open");
+    })
+}
+
+if(navMenu && navCloseBtn){
+    navCloseBtn.addEventListener("click", () =>{
+        navMenu.classList.remove("open");   
+    })
+}
+    
